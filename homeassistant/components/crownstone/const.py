885f79f5c59347bc271303d5e37474611ca8af50
RicArch97 <ricardo.steijn97@gmail.com>
2020-08-03 17:06:50 +0200

    Initial commit for device triggers

--- homeassistant/components/crownstone/const.py
+++ homeassistant/components/crownstone/const.py
@@ -1,8 +1,14 @@
 """Constants for the crownstone integration."""
 
+# Integrations
 DOMAIN = "crownstone"
+SENSOR_PLATFORM = "sensor"
+LIGHT_PLATFORM = "light"
+
+# Config flow
 CONF_SPHERE = "sphere"
 
+# Crownstone entity
 CROWNSTONE_TYPES = {
     "PLUG": "Crownstone plug",
     "CROWNSTONE_USB": "Crownstone USB",
@@ -10,12 +16,14 @@ CROWNSTONE_TYPES = {
     "BUILTIN_ONE": "Crownstone built-in one",
     "GUIDESTONE": "Crownstone guidestone",
 }
-
 CROWNSTONE_EXCLUDE = ["CROWNSTONE_USB", "GUIDESTONE"]
 
+# Presence entity
 PRESENCE_SPHERE = {"icon": "mdi:earth", "description": "Sphere presence"}
-
 PRESENCE_LOCATION = {
     "icon": "mdi:map-marker-radius",
     "description": "Location presence",
 }
+
+# Device automation
+CONF_USER = "user"
